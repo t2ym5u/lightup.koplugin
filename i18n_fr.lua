c492479038c0e06f53bc461e11a51b4a76dd61b9
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-07-20 14:19:36 +0200

    i18n: add missing FR translations

--- i18n_fr.lua
+++ i18n_fr.lua
@@ -1,3 +1,6 @@
 return {
     ["Lit: %1/%2  Bulbs: %3  Tap=cycle  Hold=dot"] = { fr = "Éclairées : %1/%2  Ampoules : %3  App.=cycle  Maintien=point" },
+    ["All cells illuminated! Puzzle solved!"] = { fr = "Toutes les cases illuminées ! Puzzle résolu !" },
+    ["Akari: place light bulbs to illuminate every white cell without conflicts."] = { fr = "Akari : placez des ampoules pour illuminer toutes les cases blanches sans conflit." },
+    ["Light Up"] = { fr = "Light Up" },
 }
